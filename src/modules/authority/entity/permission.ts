cb0c6a3ef5e5466b173f79a3e7c23452e4807c51
xiaojunnuo <xiaojunnuo@qq.com>
2021-07-02 13:59:06 +0800

    refactor: perf

--- src/modules/authority/entity/permission.ts
+++ src/modules/authority/entity/permission.ts
@@ -18,15 +18,23 @@ export class PermissionEntity {
   @Column({ comment: '权限代码', length: 100, nullable: true })
   permission: string;
 
-  @Column({name:"parent_id", comment: '父节点ID', default: -1 })
+  @Column({ name: 'parent_id', comment: '父节点ID', default: -1 })
   parentId: number;
 
   @Column({ comment: '排序号' })
   sort: number;
 
-  @Column({name:"create_time", comment: '创建时间', default: () => 'CURRENT_TIMESTAMP' })
+  @Column({
+    name: 'create_time',
+    comment: '创建时间',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
   createTime: Date;
-  @Column({name:"update_time", comment: '修改时间', default: () => 'CURRENT_TIMESTAMP' })
+  @Column({
+    name: 'update_time',
+    comment: '修改时间',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
   updateTime: Date;
 
   // @ManyToMany(type => RoleEntity, res => res.permissions)
