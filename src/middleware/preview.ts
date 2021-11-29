16a4b122dd8c5d5d486d16b1cfd2b465e2b32177
xiaojunnuo <xiaojunnuo@qq.com>
2021-11-29 23:26:42 +0800

    fix: 预览环境禁止删除

--- src/middleware/preview.ts
+++ src/middleware/preview.ts
@@ -16,14 +16,25 @@ export class PreviewMiddleware implements IWebMiddleware {
 
   resolve() {
     return async (ctx: IMidwayKoaContext, next: IMidwayKoaNext) => {
+
+      let { url, request } = ctx;
+      console.log('request query',request.query)
+
       if (!this.preview) {
         await next();
         return;
       }
-      const { url, request } = ctx;
+
       const body: any = request.body;
+      let id = body.id || request.query.id;
+      if(id != null && typeof id === 'string'){
+        id = parseInt(id)
+      }
+      if(url.indexOf("?")!==-1){
+        url = url.substring(0,url.indexOf("?"))
+      }
       const isModify = url.endsWith('update') || url.endsWith('delete');
-      const isPreviewId = body?.id < 1000;
+      const isPreviewId = id < 1000;
       if (this.preview && isModify && isPreviewId) {
         throw new PreviewException(
           '对不起，预览环境不允许修改此数据，如需体验请添加新数据'
